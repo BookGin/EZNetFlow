5ae67f67bb45e15587670b57b535371ac4b99d58
bookgin <dongsheoil@gmail.com>
2016-12-31 15:39:27 +0800

    Create UDP socket to listen NetFlow

--- netflow.py
+++ netflow.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from struct import unpack
-import datetime
+import socket
+import struct
+from datetime import datetime
 
 class NetFlowPacket:
     SIZE_OF_HEADER = 24
@@ -10,9 +11,14 @@ class NetFlowPacket:
         self.header = self._parseHeader(packet[: NetFlowPacket.SIZE_OF_HEADER])
         self.flows = self._parseFlows(packet[NetFlowPacket.SIZE_OF_HEADER: ], self.header['count'])
 
+        if len(packet[NetFlowPacket.SIZE_OF_HEADER + self.header['count'] * NetFlowPacket.SIZE_OF_FLOW: ]) != 0:
+            print('\x1b[0;30;43m', end="")
+            print("warning: > 2 netflow data in one udp packet", end='')
+            print('\x1b[0m')
+
     def _parseHeader(self, raw_data):
         attributes = ['version', 'count', 'sysuptime_msec', 'utimestamp', 'ntimstamp', 'flow_seq', 'engine_type', 'engine_id', 'sample_mode']
-        values = unpack('!HHIiiIBBH', raw_data)
+        values = struct.unpack('!HHIiiIBBH', raw_data)
         return dict(zip(attributes, values))
 
     def _parseFlows(self, flow_packets, count):
@@ -27,7 +33,7 @@ class Flow:
 
     def _parse(self, raw_data):
         attributes = ['src_ip', 'dst_ip', 'next_hop', 'input_int', 'output_int', 'packet_count', 'packet_octet', 'start_time', 'end_time', 'src_port', 'dst_port', 'tcp_flags', 'protocol', 'ip_tos', 'src_as', 'dst_as', 'src_mask', 'dst_mask']
-        values = unpack('!IIIHHIIIIHHxBBBHHBBxx', raw_data)
+        values = struct.unpack('!IIIHHIIIIHHxBBBHHBBxx', raw_data)
         dic = dict(zip(attributes, values))
 
         # Make raw integer IP become IP string
@@ -69,16 +75,28 @@ class Hosts:
         self.ips[src_ip].addTxTraffic(count, octet, last_seen)
         self.ips[dst_ip].addRxTraffic(count, octet, last_seen)
 
-packet = open('./test', 'rb').read() #[42:]
-nf = NetFlowPacket(packet)
-
 # Simple traffic accounting
-hosts = Hosts()
-for flow in nf.flows:
-    hosts.accountTraffic(flow.src_ip, flow.dst_ip, flow.packet_count, flow.packet_octet, nf.header['utimestamp'])
+listen_ip = '127.0.0.1'
+listen_port = 2055
+udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udpsocket.bind((listen_ip, listen_port))
 
-for ip, flow in hosts.ips.items():
-    tx_percent = flow.tx_octet / (flow.tx_octet + flow.rx_octet) * 100
-    print('{:<15} total_bytes:{:<10} Tx {:<5}% Rx {:<5}%, last seen:{}'.format(
-        ip, flow.tx_octet + flow.rx_octet, tx_percent, 100 - tx_percent, datetime.datetime.fromtimestamp(flow.last_seen).strftime('%Y-%m-%d %H:%M:%S')
-    ))
+hosts = Hosts()
+while True:
+    packet, (ip, port) = udpsocket.recvfrom(1024)
+    print("recv from {}:{}".format(ip, port))
+
+    try:
+        nf = NetFlowPacket(packet)
+    except struct.error:
+        print('\x1b[0;30;41m' + 'Recv corrupt packet, cannot unpack it' + '\x1b[0m')
+        continue
+
+    for flow in nf.flows:
+        hosts.accountTraffic(flow.src_ip, flow.dst_ip, flow.packet_count, flow.packet_octet, nf.header['utimestamp'])
+
+    for ip, flow in hosts.ips.items():
+        tx_percent = flow.tx_octet / (flow.tx_octet + flow.rx_octet) * 100
+        print('{:<15} total_bytes:{:<10d} Tx {:<2.2f}% Rx {:<2.2f}%, last seen:{}'.format(
+        ip, flow.tx_octet + flow.rx_octet, tx_percent, 100 - tx_percent, datetime.fromtimestamp(flow.last_seen).strftime('%Y-%m-%d %H:%M:%S')
+        ))
